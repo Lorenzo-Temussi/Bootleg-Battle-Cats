b1acd018b0f226a70df843e62bc855d42d29a76d
Lorenzo Temussi <temussilorenzo@gmail.com>
2025-10-28 16:08:34 +0100

    first commit

---
+++ demo/src/main/java/bbc/Background.java
@@ -0,0 +1,11 @@
+package bbc;
+
+public class Background {
+  private Base enemyBase;
+  private Base catBase;
+
+  public Background(Base enemyB, Base catB) {
+    this.enemyBase = enemyB;
+    this.catBase= catB;
+  }
+}
---
+++ demo/src/main/java/bbc/Base.java
@@ -0,0 +1,13 @@
+package bbc;
+
+public class Base {
+  private int hp;
+  private int position;
+  private int direction;
+
+  public Base(int hp, int position, int direction) {
+    this.hp = hp;
+    this.position = position;
+    this.direction = direction;
+  }
+}
